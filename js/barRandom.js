f4c3cfc06a80c1756956fccffdc3b5a4a526951d
Alex Dolara <alexander.dolara@gmail.com>
2022-02-27 10:34:41 +0100

    loading first approach

---
+++ js/barRandom.js
@@ -0,0 +1,11 @@
+function setProperty(duration) {
+    getComputedStyle(document.documentElement).getPropertyValue('--bar-size');
+    document.documentElement.style.setProperty('--bar-size', duration +'%');
+}
+
+function changeAnimationTime(min, max) {
+    var animationDuration = Math.random() * (max - min) + min;
+    setProperty(Math.round(animationDuration));
+}
+  
+changeAnimationTime(50, 100);
