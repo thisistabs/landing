e7fe80e099188442a953936d7dbe2e3c61de4ab7
Alex Dolara <alexander.dolara@gmail.com>
2022-06-07 09:40:55 +0200

    typo error

--- js/random.js
+++ js/random.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var classCycle=['bg1','bg2','bg3','bg4','bg5','bg6','bg7','bg8','bg9','bg10','bg11','bg12','bg13','bg14','b15'];
+    var classCycle=['bg1','bg2','bg3','bg4','bg5','bg6','bg7','bg8','bg9','bg10','bg11','bg12','bg13','bg14','bg15'];
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
     
